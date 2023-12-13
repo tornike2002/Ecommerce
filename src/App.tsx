1c90e8fda6f7c03ea9fbab60c524e5c0e91182a6
tornike2002 <toko.toka2000@gmail.com>
2023-12-13 20:00:23 +0400

    added router dom and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
-   <h1>hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
