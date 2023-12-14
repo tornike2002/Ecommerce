eda3c80b58d4aaf8e08936072af821b2a4768e60
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-12-14 08:25:30 +0400

    add pages in page folder and also pathes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="about" element={<About />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+
       </Routes>
     </>
   );
