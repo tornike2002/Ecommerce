213aa22182015163f74c37d99ab96357ae7c8492
tornike2002 <toko.toka2000@gmail.com>
2023-12-20 18:12:28 +0400

    added firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKMQKy6PVKaxeICvKNuC4Z03cBNw07YeU",
+  authDomain: "ecommerce-e1a4c.firebaseapp.com",
+  databaseURL: "https://ecommerce-e1a4c-default-rtdb.firebaseio.com",
+  projectId: "ecommerce-e1a4c",
+  storageBucket: "ecommerce-e1a4c.appspot.com",
+  messagingSenderId: "1021021677216",
+  appId: "1:1021021677216:web:cc94f4a5916bf812b68bc5",
+  measurementId: "G-3QHXRGD9J9"
+};
+
+
+const app = initializeApp(firebaseConfig);
