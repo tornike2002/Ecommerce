5b75918a6f520e8d1f1177fd22a5a080d530923c
Cotne Basiashvili <basiashvilic@gmail.com>
2023-12-27 03:27:08 +0400

    added Navigation Styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,17 @@
-import { Link } from "react-router-dom";
 import ProductsCarousel from "../components/carousel/ProductsCarousel";
 
 import styled from "styled-components";
-import { useState } from "react";
 import scrollUp from "../../src/scroll-up-svgrepo-com.svg";
 // import { OneProductCarousel } from "../components/carousel/OneProductCarousel";
 import OneProductCarousel from "../components/carousel/OneProductCarousel";
 
-
-
-
-
-
 const Home = () => {
-  const [showPageLinks, setShowPageLinks] = useState(false);
-
-
-
-  const ResetButton = styled.button`
-  /* Resetting default styles */
-  appearance: none; 
-  background: none;
-  border: none;
-  margin: 0;
-  padding: 0;
-  font: inherit;
-  cursor: pointer;
-  outline: none; 
-  color: black;
-  background-color: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-
-  &:hover {
-    color: #dc3545;
-  }
-`;
-
-  const PageLinksContainer = styled.div`
-    position: relative;
-    display: inline-block;
-  `;
-
-  const PageLinksDropdown = styled.div`
-    display: ${showPageLinks ? "block" : "none"};
-    position: absolute;
-    background-color: white;
-    border: 1px solid #ccc;
-    padding: 8px;
-    z-index: 1;
-  `;
-
   const ScrollToTopButtonContainer = styled.div`
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  z-index: 1000; /* Ensure it's above other elements */
-`;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 1000; /* Ensure it's above other elements */
+  `;
   // სკროლისთვის და ასევე ინტერვალისთვის რომ, ერთიანად არ აისქორლოს..
   const scrollToTop = () => {
     const scrollStep = -window.scrollY / (500 / 15);
@@ -69,106 +24,29 @@ const Home = () => {
       }
     }, 15);
   };
+  const ResetButton = styled.button`
+    /* Resetting default styles */
+    appearance: none;
+    background: none;
+    border: none;
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: none;
+    color: black;
+    background-color: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+
+    &:hover {
+      color: #dc3545;
+    }
+  `;
 
   return (
     <div>
-
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'centergit' }}>
-        <Link to='/'>
-          <ResetButton >
-            <h3>
-              Home
-            </h3>
-          </ResetButton>
-        </Link>
-        <Link to='/categories'>
-          <ResetButton>
-            <h3>
-              Categories
-            </h3>
-          </ResetButton>
-        </Link>
-        <Link to='/about'>
-          <ResetButton>
-            <h3>
-              About
-            </h3>
-          </ResetButton>
-        </Link>
-
-        {/* ეს მხოლოდ pages-თან რომ მაუსს მიიტან და ჩამოიშლება ჩამონათვალები.. */}
-        <PageLinksContainer
-          onMouseEnter={() => setShowPageLinks(true)}
-          onMouseLeave={() => setShowPageLinks(false)}
-        >
-          <h3>Pages</h3>
-          <PageLinksDropdown>
-            {/* Links for Login, Cart, etc. */}
-            <Link to='/login'>
-              <ResetButton>
-                <h3>
-                  Login
-                </h3>
-              </ResetButton>
-            </Link>
-            <Link to='/cart'>
-              <ResetButton>
-                <h3>
-                  Cart
-                </h3>
-              </ResetButton>
-            </Link>
-            <Link to='/checkout'>
-              <ResetButton>
-                <h3>
-                  Checkout
-                </h3>
-              </ResetButton>
-            </Link>
-            <Link to='/bookDetails'>
-              <ResetButton>
-                <h3>
-                  Book Details
-                </h3>
-              </ResetButton>
-            </Link>
-            <Link to='/blogDetails'>
-              <ResetButton>
-                <h3>
-                  Blog Details
-                </h3>
-              </ResetButton>
-            </Link>
-            <Link to='/element'>
-              <ResetButton>
-                <h3>
-                  Element
-                </h3>
-              </ResetButton>
-            </Link>
-          </PageLinksDropdown>
-        </PageLinksContainer>
-
-        <Link to='/blog'>
-          <ResetButton>
-            <h3>
-              Blog
-            </h3>
-          </ResetButton>
-        </Link>
-        <Link to='/contact'>
-          <ResetButton>
-            <h3>
-              Contact
-            </h3>
-          </ResetButton>
-        </Link>
-      </div>
-
-
-
       <OneProductCarousel />
-
       <ProductsCarousel />
       <ProductsCarousel />
       <ProductsCarousel />
@@ -181,14 +59,13 @@ const Home = () => {
       <ProductsCarousel />
 
       {/* სქროლი, რომ მაღლა აგაგდოს */}
-      <div  >
+      <div>
         <ScrollToTopButtonContainer>
-          <ResetButton onClick={scrollToTop} style={{ borderRadius: '30px' }} >
-            <img src={scrollUp} alt='logo' style={{ width: '40px' }} />
+          <ResetButton onClick={scrollToTop} style={{ borderRadius: "30px" }}>
+            <img src={scrollUp} alt="logo" style={{ width: "40px" }} />
           </ResetButton>
         </ScrollToTopButtonContainer>
       </div>
-
     </div>
   );
 };
