25b773bfdcdaf2c917915165896ecab169ef7702
tornike2002 <toko.toka2000@gmail.com>
2023-12-27 20:58:58 +0400

    fixed some errors in titleandimg.tsx

--- src/components/pageTitleAndBgImage/TitleAndImg.tsx
+++ src/components/pageTitleAndBgImage/TitleAndImg.tsx
@@ -1,5 +1,5 @@
-import "./TitleAndImgStyles.css";
 
+import styled from "styled-components";
 type Props = {
   title: string;
   link: string;
@@ -7,10 +7,27 @@ type Props = {
 
 const TitleAndImg = ({ title, link }: Props) => {
   return (
-    <div id="a" style={{ backgroundImage: `url(${link})` }}>
-      <h2 id="b">{title}</h2>
-    </div>
+    <HeroForBlog id="a" style={{ backgroundImage: `url(${link})` }}>
+      <TitleForHero>{title}</TitleForHero>
+    </HeroForBlog>
   );
 };
 
 export default TitleAndImg;
+
+const TitleForHero = styled.h2`
+  color: #fff;
+  font-size: 50px;
+  display: block;
+  font-weight: 400;
+  line-height: 1.4;
+  margin: 0 auto;
+`;
+const HeroForBlog = styled.div`
+  height: 350px;
+  background-repeat: no-repeat;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  background-position: center;
+`;
