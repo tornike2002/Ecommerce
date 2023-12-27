c7bc8a58cafe7b9512af1f7a5b20d265553d756a
tornike2002 <toko.toka2000@gmail.com>
2023-12-27 21:17:14 +0400

    transfer inline styles to styling components

--- src/components/styledComponents/containers.ts
+++ src/components/styledComponents/containers.ts
@@ -4,7 +4,4 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1140px;
   margin: 0 auto;
-  /* display: flex;
-  justify-content: center;
-  align-items: center; */
 `;
