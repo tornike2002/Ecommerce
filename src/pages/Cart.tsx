43c34b53c6e423210b12308e8b348f1a868a8f77
tornike2002 <toko.toka2000@gmail.com>
2023-12-30 16:02:22 +0400

    added empty cart problem solver

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,6 +5,7 @@ import SaleBook from "../components/saleBook/SaleBook";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Container } from "../components/styledComponents/containers";
+import { Button, Result } from 'antd';
 
 export default function Cart() {
   const books = useRecoilValue(cart);
@@ -12,6 +13,10 @@ export default function Cart() {
   const CheckoutHandler = () => {
     navigate("/checkout");
   };
+
+  const emptyCardHandler = () => {
+    navigate("/categories")
+  }
 
   return (
     <Container>
@@ -21,31 +26,48 @@ export default function Cart() {
           link="https://preview.colorlib.com/theme/abcbook/assets/img/hero/h2_hero2.jpg"
         />
       </div>
-      <ul>
-        {books.map((book: Book) => (
-          <SaleBook
-            key={book.id}
-            img={book.img}
-            price={book.price}
-            title={book.title}
-          />
-        ))}
-      </ul>
+      {books.length === 0 ? (
+         <Result
+         title="Cart is Empty please add items..."
+         extra={
+           <Button type="primary" key="console" onClick={emptyCardHandler}>
+             Go to Categories
+           </Button>
+         }
+       />
+      ) : (
+        <ul>
+          {books.map((book: Book) => (
+            <SaleBook
+              key={book.id}
+              img={book.img}
+              price={book.price}
+              title={book.title}
+            />
+          ))}
+        </ul>
+      )}
       <CartCheckoutButton type="button" onClick={CheckoutHandler}>
         Go to checkout
       </CartCheckoutButton>
     </Container>
   );
 }
-
+const EmptyCart = styled.h1`
+  
+    font-family: "Roboto", sans-serif;
+    font-size: 25px;
+    text-align: center;
+    display: block;
+`
 const CartCheckoutButton = styled.button`
-  background-color: #FF1616;
-  font-family: 'Roboto', sans-serif;
+  background-color: #ff1616;
+  font-family: "Roboto", sans-serif;
   padding: 12px 23px;
   cursor: pointer;
   color: #fff;
   font-size: 16px;
   font-weight: 500;
-  border-radius:36px;
+  border-radius: 36px;
   margin-top: 30px;
-`
+`;
