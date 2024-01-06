8bcba325d45e93b87cbaade7953bb89ddb067d8b
tornike2002 <toko.toka2000@gmail.com>
2024-01-06 22:35:25 +0400

    added container and bg container to checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components";
 import CheckoutPage from "../components/checkoutForm/CheckoutPage";
+import { Container } from "../components/styledComponents/containers";
 
 const Checkout = () => {
-  return <CheckoutPage />;
+  return (
+    <CheckoutBgContainer>
+      <Container>
+        <CheckoutPage />
+      </Container>
+    </CheckoutBgContainer>
+  );
 };
 
 export default Checkout;
+
+const CheckoutBgContainer = styled.div`
+  background-color: #F2F2F2;
+`
