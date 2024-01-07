cf15414244ac7a6192a9d14941f6157fadd54fad
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2024-01-07 04:28:28 +0400

    added some styles

--- src/components/aboutPage/AboutPage.tsx
+++ src/components/aboutPage/AboutPage.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { } from "styled-components";
 import TitleAndImg from "../pageTitleAndBgImage/TitleAndImg";
 import { Container } from "../styledComponents/containers";
 import { Rate } from "antd";
@@ -10,50 +10,28 @@ const AboutPage = () => {
   const text =
     "Beryl Cook is one of Britain’s most talented and amusing artists..."; // Your text here
 
-  // Define a styled component for the animated container
   const KenBurnsContainer = styled(motion.div)`
-    /* Apply the animation properties to this container */
-    @-webkit-keyframes kenburns-top {
-      0% {
-        -webkit-transform: scale(1) translateY(0);
-        transform: scale(1) translateY(0);
-        -webkit-transform-origin: 50% 16%;
-        transform-origin: 50% 16%;
-      }
-      100% {
-        -webkit-transform: scale(1.25) translateY(-15px);
-        transform: scale(1.25) translateY(-15px);
-        -webkit-transform-origin: top;
-        transform-origin: top;
-      }
-    }
     @keyframes kenburns-top {
       0% {
-        -webkit-transform: scale(1) translateY(0);
         transform: scale(1) translateY(0);
-        -webkit-transform-origin: 50% 16%;
         transform-origin: 50% 16%;
       }
       100% {
-        -webkit-transform: scale(1.25) translateY(-15px);
         transform: scale(1.25) translateY(-15px);
-        -webkit-transform-origin: top;
         transform-origin: top;
       }
     }
-    
-    /* Apply the animation with a single iteration */
-  -webkit-animation: kenburns-top 5s ease-out both 1;
-  animation: kenburns-top 5s ease-out both 1;
-`;
 
+    @media screen and (min-width: 1200px) {
+      animation: kenburns-top 5s ease-out both 1;
+    }
+  `;
 
   return (
     <div>
       <Container>
         <TitleAndImg title="About" link={imgSrc} />
 
-        {/* Apply the Ken Burns effect to the animated container */}
         <KenBurnsContainer
           initial={{ x: -50, opacity: 0 }}
           transition={{ duration: 1 }}
@@ -67,7 +45,6 @@ const AboutPage = () => {
           <Rate allowHalf defaultValue={2.5} />
         </KenBurnsContainer>
 
-        {/* Similarly, apply the effect to the next section */}
         <KenBurnsContainer
           initial={{ x: -50, opacity: 0 }}
           transition={{ duration: 1 }}
@@ -97,6 +74,7 @@ const AboutTitle = styled.h1`
   font-family: "Playfair Display";
   margin-top: 50px;
 `;
+
 const AboutParagraph = styled.p`
   color: #707070;
   font-size: 16px;
