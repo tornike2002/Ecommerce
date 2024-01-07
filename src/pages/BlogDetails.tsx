2dc99f6590958ccff2fad4149166eea7d3a69c14
Cotne Basiashvili <basiashvilic@gmail.com>
2024-01-07 13:57:09 +0400

    removed unuseful pages

--- src/pages/BlogDetails.tsx
+++
@@ -1,5 +0,0 @@
-const BlogDetails = () => {
-  return <div>BlogDetails</div>;
-};
-
-export default BlogDetails;
--- src/pages/BookDetails.tsx
+++
@@ -1,5 +0,0 @@
-const BookDetails = () => {
-  return <div>BookDetails</div>;
-};
-
-export default BookDetails;
