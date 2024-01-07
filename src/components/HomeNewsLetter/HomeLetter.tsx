a1313b5840066ea97c758706b44ab3cb6ea8e9eb
tornike2002 <toko.toka2000@gmail.com>
2024-01-07 23:05:12 +0400

    added background cover

--- src/components/HomeNewsLetter/HomeLetter.tsx
+++ src/components/HomeNewsLetter/HomeLetter.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+import ban3 from "../../assets/homeBanner/ban3.jpg.webp";
 const HomeLetter = () => {
-  return <div>hello</div>;
+  return <HomeLetterMainContainer></HomeLetterMainContainer>;
 };
 
 export default HomeLetter;
+
+const HomeLetterMainContainer = styled.div`
+  width: 100%;
+  min-height: 351px;
+  background: url(${ban3});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  margin-top: 30px;
+`;
